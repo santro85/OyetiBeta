3b3519f5a840efdd0db3b4eaab329785f0241e2b
santro85 <45917354+santro85@users.noreply.github.com>
2019-01-17 11:25:18 +0530

    Add files via upload

---
+++ assets/ServiceWorker.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = 'Oyeti';
+//new
+var urlsToCache = [
+  '/',
+  'style.css',
+  'assets/app.js',
+  'assets/icons/icon-256x256.png',
+  'assets/bootstrap/jsbootstrap.min.js',
+  'assets/bootstrap/cssbootstrap.min.css',
+  'assets/Jquery/jquery-3.3.1.min.js'
+];
+
+
+self.addEventListener('install', function (event) {
+  console.log("installing")
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache).then(function () {
+          console.log('All resources have been fetched and cached.');
+        });
+      })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+
+    })
+  );
+
+});
